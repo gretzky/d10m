8def7f3d09a31810e05344e6eed3127d4379c6ef
gretzky <sfed617@gmail.com>
2018-11-17 15:43:57 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-export { Text, Dfn, A, Heading } from './components/Text'
-export { Small, Sub, Sup } from './components/Text/styles'
 export { default as Global } from './styles/global'
 export { default as theme } from './styles/theme'
